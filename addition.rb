eeb2f1f9cbc6c640a206e7a6cef74ebe4b94934f
vcmccormick <vcmccormick@gmail.com>
2017-07-12 11:27:41 -0400

    getting addition to work

--- addition.rb
+++ addition.rb
@@ -3,6 +3,11 @@ y = 2 + 2
 z = 3 + 3
 a = 1 + 1 + 2 + 2 + 3 + 3
 
+def add(a, b)
+	p "ADDING #{a} + #{b}"
+	return a + b
+end
+
 p "1 + 1 = #{x}"
 
 p "2 + 2 = #{y}"
@@ -11,3 +16,4 @@ p "3 + 3 = #{z}"
  
 p "#{x} + #{y} + #{z} = #{a}"
 
+print add(6,7)
